3e55a18930fb6eada23e2ad394c5ce477628fc41
Rodrigo Lins <rlinsdev@gmail.com>
2021-05-03 16:43:13 -0300

    Update pessoa

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,8 @@
 const bodyParser = require('body-parser')
-const pessoas = require('./pessoasRouter')
+const pessoas = require('./pessoaRouter')
 
 module.exports = app => {
   app.use(bodyParser.json())
   app.get(pessoas)
-  app.get('/', (req, res) => res.send('olá'))
+  app.get('/', (req, res) => res.send('olá 1'))
 }
--- api/routes/pessoaRouter.js
+++ api/routes/pessoaRouter.js
@@ -4,5 +4,8 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 
 module.exports = router
